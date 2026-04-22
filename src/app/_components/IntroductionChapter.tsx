bb744b98462762449b57d73cd738b4c72ab7068f
vuezy <zoltandididiop0@gmail.com>
2026-04-22 07:02:16 +0700

    feat: add profile picture

--- src/app/_components/IntroductionChapter.tsx
+++ src/app/_components/IntroductionChapter.tsx
@@ -4,6 +4,7 @@ import JavaScriptIcon from "@/components/icons/JavaScriptIcon";
 import LaravelIcon from "@/components/icons/LaravelIcon";
 import TypeScriptIcon from "@/components/icons/TypeScriptIcon";
 import profile from "@/data/profile.json";
+import Image from "next/image";
 
 export default function IntroductionChapter() {
   const lastUpdated = new Date('2026-04-19');
@@ -16,8 +17,8 @@ export default function IntroductionChapter() {
       lastUpdated={lastUpdated}
       nextChapterId="about-me"
     >
-      <div className="grid grid-cols-1 md:grid-cols-[1fr_220px] items-center gap-12 mb-10">
-        <div className="flex flex-col order-2 md:order-1">
+      <div className="grid grid-cols-1 sm:grid-cols-[1fr_200px] items-center gap-12 mb-10">
+        <div className="flex flex-col order-2 sm:order-1">
           <em className="font-medium tracking-[-0.02rem] mb-2.5 text-3xl xs:text-5xl md:text-6xl text-primary">{profile.name}</em>
 
           <div className="flex items-center gap-2 text-lg text-muted-foreground font-light">
@@ -48,9 +49,10 @@ export default function IntroductionChapter() {
           </div>
         </div>
 
-        <div className="flex items-center justify-center gap-3 order-1 md:order-2 md:items-start md:justify-start">
-          <div className="w-44 h-44 bracket-frame">
-            <div className="w-full h-full bg-card border rounded-lg flex items-center justify-center font-serif text-[36px] font-medium italic">S</div>
+        <div className="flex items-center justify-center gap-3 order-1 sm:order-2 sm:items-start sm:justify-start">
+          <div className="w-44 h-50 sm:w-44 sm:h-56 md:w-48 md:h-60 bracket-frame">
+            <Image src="/profile.jpeg" alt="" width={192} height={240} className="block dark:hidden w-full h-full object-cover object-top" />
+            <Image src="/profile-dark.jpeg" alt="" width={192} height={240} className="hidden dark:block w-full h-full object-cover object-top" />
           </div>
         </div>
       </div>
